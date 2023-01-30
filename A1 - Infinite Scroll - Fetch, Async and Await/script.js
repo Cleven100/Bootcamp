336f076dacf230a14ba79fd131d68a7c20d5c8ce
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2023-01-30 15:45:09 -0300

    Filter

--- A1 - Infinite Scroll - Fetch, Async and Await/script.js
+++ A1 - Infinite Scroll - Fetch, Async and Await/script.js
@@ -2,13 +2,16 @@ const postsContainer = document.getElementById('post-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
-let limit = 3;
+let limit = 5;
 let page = 1;
+let globalData;
+
 
 (async () => { 
     let res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
     let data = await res.json();
     showPosts(data);
+    globalData = data;
 })();
 
 
@@ -32,6 +35,53 @@ function showPosts(data) {
    postsContainer.appendChild(postEl);
 
 })
+}
 
+
+function showLoading(data) {
+    loading.classList.add('show'); 
+
+    setTimeout(() => {
+         
+        setTimeout(()=> {
+            page++;
+            (async () => { 
+                let res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
+                let data = await res.json();
+                showPosts(data);
+                globalData = data;
+            })();
+        }, 300)
+    }, 1000);
+}
+
+
+function filterPosts(e) {
+ const term = e.target.value.toUpperCase();
+ const posts = document.querySelectorAll('.post');
+
+
+ posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if(title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex'
+    } else {
+        post.style.display = 'none';
+    }
+ });
 
 }
+
+
+function handleScroll() {
+    const {scrollTop, scrollHeight, clientHeight} =  document.documentElement;
+  
+    if(scrollTop + clientHeight >= scrollHeight - 5) {
+      showLoading(globalData);
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
+filter.addEventListener('input', filterPosts);
